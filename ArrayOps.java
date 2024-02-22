254a54e2071d08b57d6f91c84d9b290ccc20b9fb
ofirmatz <ofir.matz@post.runi.ac.il>
2024-02-22 12:01:07 +0200

    check

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,89 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+    
+        System.out.println(findMissingInt(new int[] {0, 1, 2, 3, 4, 6}));  
+        System.out.println(secondMaxValue(new int[] {6, 9, 4, 13, 3, 4})); 
+        System.out.println(containsTheSameElements(new int[] {1, 2, 1, 1, 2}, new int[] {2, 1}));
+        System.out.println(isSorted(new int[] {1, 0, -1, 0, -3}));
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int n = array.length;
+        int expectedSum = (n * (n+1))/2;
+        int actualSum = 0;
+        int missingNumber = 0;
+        for(int i = 0; i < array.length; i++){
+                actualSum = actualSum + array[i];
+                 }
+            missingNumber = expectedSum - actualSum;
+        return missingNumber;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int max = array[0];
+        int secondMax = 0;
+        for(int i = 0; i < array.length; i++){
+            if(array[i] > max){
+                secondMax = max;
+                max = array[i];
+            }else if((array[i] > secondMax) && (array[i] < max)){
+                secondMax = array[i];
+            }
+       }
+        return secondMax;
+    }
+   // System.out.println(containsTheSameElements(new int[] {1, 2, 1, 1, 2}, new int[] {2, 1}));
+  
+   public static boolean containsTheSameElements(int [] array1,int [] array2) {
+        return containsOneWay(array1, array2) && containsOneWay(array2, array1);
     }
 
-    public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
-    }
 
-    public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
-    }
 
-}
+
+    public static boolean containsOneWay(int[] arr1, int[] arr2) {
+        boolean sameElement = false;
+
+        for(int i = 0; i < arr1.length; i++){
+            sameElement = false;
+            for(int j = 0; j < arr2.length; j++){
+                if(arr1[i] == arr2[j]){
+                    sameElement = true;
+                    break;
+                }
+            }
+            if(sameElement == false){
+                return sameElement;
+            }
+        }
+
+        return true;
+    }
+    public static boolean isSorted(int [] array){
+        int i = 0;
+        while ((i < array.length - 1) && (array[i] == array[i+1])){
+            i++;
+        }
+        boolean checkIfGoingUp = array[i]-array[i+1] < 0;
+        boolean isSorted = false;
+
+        
+        for( int j = i ; j < array.length - 1; j++){
+            if (checkIfGoingUp){
+                if(array[j] > array[j+1]){
+                    isSorted = false;
+                    return isSorted;
+                }
+            }else{
+                 if(array[j] < array[j+1]){
+                   isSorted = false;
+                   return isSorted;
+               }
+            }
+
+         }
+         isSorted = true;
+         return isSorted;
+    } 
+}   
+
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,104 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        System.out.println(capVowelsLowRest("Hello World"));
+        System.out.println(camelCase("  HELLo  world"));
+        System.out.println(allIndexOf("hello world", 'l'));
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
-    }
+        String isSolution = "";
+        
+        for (int j = 0; j < string.length(); j++) {
+            char currentChar = string.charAt(j);
 
+            if(currentChar=='a' || currentChar=='i' || currentChar=='e' ||
+             currentChar=='o' || currentChar=='u'){
+                isSolution += (char)(currentChar - 32);
+            } else if(currentChar=='A' || currentChar=='I' || currentChar=='E' ||
+             currentChar=='O' || currentChar=='U')  {
+                isSolution += currentChar;
+
+
+            } else if (currentChar >= 'A' && currentChar <= 'Z') {
+            isSolution +=  (char)(currentChar + 32);  
+ 
+        } else {
+            isSolution += currentChar; 
+
+       }
+
+   }
+        return isSolution;
+
+ }
+
+    
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
-    }
+            String isSolution = "";
+            boolean isFirst = false;
+    
+            for (int i = 0; i < string.length(); i++) {
+                char currentChar = string.charAt(i);
+    
+                // space case (ignore)
+                // if (currentChar == ' ') {
+                // continue;
+                // }
+    
+                if (i > 0) {
+                    // first char at word that isn't the first word (need to be upper case)
+                    if (currentChar > ' ' && string.charAt(i - 1) == ' ' && isFirst) {
+                        if (currentChar >= 'a' && currentChar <= 'z') {
+                            isSolution += (char) (currentChar - 32);
+                        } else {
+                            isSolution += currentChar;
+                        }
+                        // continue;
+                    }
+    
+                    if (currentChar > ' ' && string.charAt(i - 1) > ' ' && i > 0) {
+                        // any other char (need to be lower case)
+                        if (currentChar >= 'A' && currentChar <= 'Z') {
+                            isSolution += (char) (currentChar + 32);
+                        } else {
+                            isSolution += currentChar;
+                        }
+                    }
+                }
+    
+                // first letter in the string
+                if (currentChar > ' ' && !isFirst) {
+                    isFirst = true;
+                    if (currentChar >= 'A' && currentChar <= 'Z') {
+                        isSolution += (char) (currentChar + 32);
+                    } else {
+                        isSolution += currentChar;
+                    }
+                    // continue;
+                }
+            }
+    
+            return isSolution;
+        }
+   
 
-    public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
-    }
+        public static int[] allIndexOf(String string, char chr) {
+            int count = 0;
+            for (int i = 0; i < string.length(); i++) {
+                if (string.charAt(i) == chr) {
+                    count++;
+                }
+            }
+        
+            int[] isSolution = new int[count];
+            int index = 0;
+            for (int i = 0; i < string.length(); i++) {
+                if (string.charAt(i) == chr) {
+                    isSolution[index] = i;
+                    index++;
+                }
+            }
+            return isSolution;
+        }  
 }
